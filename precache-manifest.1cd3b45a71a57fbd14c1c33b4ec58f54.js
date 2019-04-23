c0d3d2d3c19e0b440ad6c95b0b6271a101787ab5
tbsse <49879187+tbsse@users.noreply.github.com>
2019-04-22 17:31:18 -0700

    Updates

---
+++ precache-manifest.1cd3b45a71a57fbd14c1c33b4ec58f54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "6efb3dd8c44dfc10ef1d",
+    "url": "/my-app/static/js/main.3ff91dbc.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/my-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6efb3dd8c44dfc10ef1d",
+    "url": "/my-app/static/css/main.e9f00b41.chunk.css"
+  },
+  {
+    "revision": "9cbb7fdaa288e1cb7ca2370e08a705ce",
+    "url": "/my-app/index.html"
+  }
+];
