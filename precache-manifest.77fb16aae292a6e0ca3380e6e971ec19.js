f3aab1183d5a0d87e220830dedf4794cdcf938a6
tbsse <49879187+tbsse@users.noreply.github.com>
2019-04-30 16:55:27 -0700

    Updates

---
+++ precache-manifest.77fb16aae292a6e0ca3380e6e971ec19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25384a44738a507f3db2ca023cd06518",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9c235ad97dbb84c1c9d5",
+    "url": "/my-app/static/css/main.28f74f55.chunk.css"
+  },
+  {
+    "revision": "23da850bdda19e0ac640",
+    "url": "/my-app/static/js/2.aba73edb.chunk.js"
+  },
+  {
+    "revision": "9c235ad97dbb84c1c9d5",
+    "url": "/my-app/static/js/main.fc955900.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
