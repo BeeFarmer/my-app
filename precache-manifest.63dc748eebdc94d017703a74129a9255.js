9ea2a632767effacb5eadfe0290f613cab152e1d
tbsse <49879187+tbsse@users.noreply.github.com>
2019-05-02 15:42:26 -0700

    Updates

---
+++ precache-manifest.63dc748eebdc94d017703a74129a9255.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d0cf70de4beb0e6eab9d73f0268f35",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c21a3901d7a9c6b01c77",
+    "url": "/my-app/static/css/main.ee2cc92f.chunk.css"
+  },
+  {
+    "revision": "0541286634a36ea0013a",
+    "url": "/my-app/static/js/2.45ecd894.chunk.js"
+  },
+  {
+    "revision": "c21a3901d7a9c6b01c77",
+    "url": "/my-app/static/js/main.84180a1b.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.e6d612fd863a18d10d354a070ba18c7b.js"
+  "/my-app/precache-manifest.63dc748eebdc94d017703a74129a9255.js"
 );
 
 self.addEventListener('message', (event) => {
